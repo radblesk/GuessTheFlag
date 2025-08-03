70baca7b0a57ea7a66ada98806aa0f9ef7c8de05
radblesk <144258807+radblesk@users.noreply.github.com>
2025-08-03 14:30:57 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Radoslav Bley on 03/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
